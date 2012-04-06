79a7109c0116e08576ad42992b88f84d72efde3e
xiaozhou <xinghua.zhou@gmail.com>
2012-04-06 17:16:24 +0800

    adspot js init

---
+++ adspot-js/adspot.js
@@ -0,0 +1,36 @@
+
+(function() {
+	var startAdspot =  function($){
+		
+		//查找所有图片(img),过滤不符合尺寸的图片
+		(function(){
+			$("img").each(function(i){
+				var _self = $(this);
+				var width = _self.width();
+				var height = _self.height();
+				
+				alert(width + "x" + height);
+			});
+		})();
+		
+		
+	};
+	
+	(function() {
+		var jqueryScript, clearFn;
+		if(typeof jQuery !== "undefined" && jQuery.fn.jquery === "1.7.1"){
+			startAdspot(jQuery);
+		}else{
+			jqueryScript = document.createElement("script");
+			jqueryScript.src = "http://ajax.googleapis.com/ajax/libs/jquery/1.7.1/jquery.min.js";
+			document.documentElement.getElementsByTagName("HEAD")[0].appendChild(jqueryScript);
+			clearFn = setInterval(
+				function() {
+					if(typeof jQuery !== "undefined" && jQuery.fn.jquery === "1.7.1"){
+						clearInterval(clearFn);
+						startAdspot(jQuery);
+					}
+				}, 25);
+		}
+	})();
+})();

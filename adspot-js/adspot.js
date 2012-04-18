575e4dad9bf2a8225e984a1e6498d2c31d3b5f48
javaside <xinghua.zhou@gmail.com>
2012-04-18 22:44:29 +0800

    test

--- adspot-js/adspot.js
+++ adspot-js/adspot.js
@@ -1,9 +1,28 @@
 
 (function() {
-	var startAdspot =  function($){
+	var wbCode = window['__asspot_wb_code'];
+	
+	var startAdspot =  function($){	
 		
-		var initImg = function(img){
+		var showAdSpot =  function(img){
 			
+		}
+		
+		var hideAdSpot = function(img){
+			
+		}
+		
+		//初始化图的锚点
+		var initImg = function(img){
+			img.hover(
+					function(){
+						showAdSpot(img);
+					},
+					
+					function(){
+						hideAdSpot(img);
+					}
+			);
 		};
 		
 		//查找所有图片(img),过滤不符合尺寸的图片
@@ -21,6 +40,8 @@
 	};
 	
 	(function() {
+		if(!wbCode){return;};
+		
 		var jqueryScript, clearFn;
 		if(typeof jQuery !== "undefined" && jQuery.fn.jquery === "1.7.1"){
 			startAdspot(jQuery);

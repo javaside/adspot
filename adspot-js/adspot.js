95cdb33f98d2e1629be59c167d261c0d67ef7e8d
javaside <xinghua.zhou@gmail.com>
2012-04-23 23:38:21 +0800

    打点

--- adspot-js/adspot.js
+++ adspot-js/adspot.js
@@ -24,6 +24,11 @@
 			return {dots:[{left:20,top:30},{left:10,top:80},{left:90,top:100}]};
 		}
 		
+		var addDotImgHtml = function(left, top, show){
+			var display = (show) ? "display:inline;" : "display:none;";
+			return "<img class='adSpotProductDot' style='" + display + "z-index:1699;position:absolute;margin:0px;padding:0px;left:" + left + "px;top:" + top + "px;' src='" + imgHost + "res/product.png'>";
+		}
+		
 		//包装图片的锚点
 		var wrapperProductDots = function(img){
 			var dotObjs = getProductDots(img);
@@ -32,7 +37,7 @@
 				$.each(dotObjs.dots, function(i, addot){
 					var left = addot.left;
 					var top = addot.top;
-					var dotImg = "<img class='adSpotProductDot' style='display:none;z-index:1699;position:absolute;margin:0px;padding:0px;left:" + left + "px;top:" + top + "px;' src='" + imgHost + "/res/product.png'>";
+					var dotImg = addDotImgHtml(left, top, false);
 					img.after(dotImg);
 				});
 			}
@@ -44,11 +49,22 @@
 			var height = img.height();
 			
 			var divWrap = "<div class='adSpotImgWrap' style='width:" + width + "px;height:" + height + "px;position: relative; margin:0px;float: none; padding:0px'></div>";
-		    var beaconImg = "<img class='adSpotBeaconImg' style='display:none;z-index:1700;position:absolute;margin:0px;padding:0px;top:5px;right:5px;' src='" + imgHost +"/res/metadata-beacon.png'>";
+		    var beaconImg = "<img class='adSpotBeaconImg' style='display:none;z-index:1700;position:absolute;margin:0px;padding:0px;top:5px;right:5px;' src='" + imgHost +"res/metadata-beacon.png'>";
 		    
 			img.wrap(divWrap);
 			wrapperProductDots(img);
 			img.after(beaconImg);
+		}
+	
+		var addCreateAdspotEvent = function(img){
+			img.click(function(e){
+				var left = e.pageX - img.offset().left -12;//e.originalEvent.x - img.offset().left || e.originalEvent.layerX - img.offset().left || 0;//获取当前鼠标相对img的x坐标
+			    var top  = e.pageY - img.offset().top -12;//e.originalEvent.y - img.offset().top  || e.originalEvent.layerY - img.offset().top  || 0;//获取当前鼠标相对img的y坐标
+			    
+			    var dotImg = addDotImgHtml(left, top, true);
+			    
+			    img.after(dotImg);
+			});
 		}
 		
 		//初始化图的锚点
@@ -64,6 +80,8 @@
 						hideAdSpot(img);
 					}
 			);
+			
+			addCreateAdspotEvent(img);
 		};
 		
 		//查找所有图片(img),过滤不符合尺寸的图片

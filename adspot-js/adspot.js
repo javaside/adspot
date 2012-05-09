e521a73efe58be002b54015a855ba2db6fe01465
javaside <xinghua.zhou@gmail.com>
2012-05-09 14:31:39 +0800

    显示编辑锚点DIV，锚点HOVER加亮

--- adspot-js/adspot.js
+++ adspot-js/adspot.js
@@ -9,7 +9,7 @@
 		var showAdSpot =  function(img){
 			var p = img.parent();
 			p.find(".adspot_btn_logo").show();
-			p.find(".adSpotProductDot").show();
+			p.find(".adspot_icon_link").show();
 			p.find(".adspot_btn_add_dot").show();
 		}
 		
@@ -17,7 +17,7 @@
 		var hideAdSpot = function(img){
 			var p = img.parent();
 			p.find(".adspot_btn_logo").hide();
-			p.find(".adSpotProductDot").hide();
+			p.find(".adspot_icon_link").hide();
 			p.find(".adspot_btn_add_dot").hide();
 		}
 		
@@ -26,9 +26,9 @@
 			return {dots:[{left:20,top:30},{left:10,top:80},{left:90,top:100}]};
 		}
 		
-		var addDotImgHtml = function(left, top, show){
-			var display = (show) ? "display:inline;" : "display:none;";
-			return "<img class='adSpotProductDot' style='" + display + "z-index:1699;position:absolute;margin:0px;padding:0px;left:" + left + "px;top:" + top + "px;' src='" + resHost + "res/icon_shop.png'>";
+		var addDotImgDiv = function(left, top, clas, show){
+			var display = (show) ? "display:block;" : "display:none;";
+			return "<div class='" + clas + "' style='" + display + "left:" + left + "px;top:" + top + "px;opacity:0.7;'></div>";
 		}
 		
 		//包装图片的锚点
@@ -39,34 +39,59 @@
 				$.each(dotObjs.dots, function(i, addot){
 					var left = addot.left;
 					var top = addot.top;
-					var dotImg = addDotImgHtml(left, top, false);
+					var dotImg = addDotImgDiv(left, top, "adspot_icon_link", false);
 					img.after(dotImg);
 				});
 			}
 		}
 		
+		/**
+		 * 绑定sopt编辑DIV的取消和提交按钮事件
+		 */
+		var bindSoptDivButEvent = function(img){
+			var warDiv = img.parent();
+			
+			warDiv.find(".adspot_tab_action .cancel").click(function(){
+				var warDiv = $(this).parent().parent().parent();
+				warDiv.find("div.adspot_layer_info").hide();
+				warDiv.find(".adspot_icon_space").remove();
+				bindCreateAdspotEvent(warDiv.find("img.adSpotImgWrap"));
+			});
+			
+			warDiv.find(".adspot_tab_action .submit").click(function(){
+				warDiv.find(".adspot_icon_space").attr("class", "adspot_icon_link");
+				warDiv.find("div.adspot_layer_info").hide();
+			});
+		}
 		
+		/**
+		 * 绑定左上角处于编辑状态时的事件。 
+		 */
 		var bindCreateAdspotEvent = function(img){
 			img.click(function(e){
 				var left = e.pageX - img.offset().left -12;//e.originalEvent.x - img.offset().left || e.originalEvent.layerX - img.offset().left || 0;//获取当前鼠标相对img的x坐标
 			    var top  = e.pageY - img.offset().top -12;//e.originalEvent.y - img.offset().top  || e.originalEvent.layerY - img.offset().top  || 0;//获取当前鼠标相对img的y坐标
 			    
-			    var dotImg = addDotImgHtml(left, top, true);
+			    var dotImg = addDotImgDiv(left, top, "adspot_icon_space", true);
 			    img.after(dotImg);
 			    
 			    var addDiv = img.parent().find(".adspot_layer_info");
 			    addDiv.css("left", (left + 25) + "px");
 			    addDiv.css("top", (top + 12) + "px");
 			    
-			    img.css('cursor','default');
-				img.unbind('click');
+			    unbindCreateAdspotEvent(img);
 				
 			    addDiv.show();
+			    
+			    bindSoptDivButEvent(img);
 			});
 			
 			img.css('cursor','crosshair');
 		}
 		
+		/**
+		 * 取消spot编辑状态
+		 */
 		var unbindCreateAdspotEvent = function(img){
 			img.css('cursor','default');
 			img.unbind('click');
@@ -80,6 +105,7 @@
 		var bindAddSpotDivClick = function(img){
 			img.parent().find(".adspot_btn_add_dot").click(function(e){
 				var _self = $(this);
+				
 				if(_self.hasClass("adspot_btn_add_clicked")){
 					_self.removeClass("adspot_btn_add_clicked");
 					unbindCreateAdspotEvent(img);
@@ -91,6 +117,25 @@
 			});
 		}
 		
+		/**
+		 * 绑定锚点悬浮事件
+		 */
+		var bindSoptHoverEvent = function(img){
+			var wraDiv = img.parent();
+			wraDiv.find("div.adspot_icon_link").hover(
+					function(){
+						$(this).css("opacity", "1");
+					}
+					,
+					function(){
+						$(this).css("opacity", "0.7");
+					}
+				);
+		}
+		
+		/**
+		 * 构建增加spot表单DIV
+		 */
 		var getAddEditSpotFormDiv = function(){
 			var spotForm = '<div class="adspot_layer_info">' +
 						   '<div class="adspot_layer_inner">' +
@@ -116,7 +161,7 @@
 						   '<li class="adspot_search_addproduct"><p>找不到你需要的商品?</p> <a href="#" class="adspot_edit_btn adspot_add_btn"><i></i> 添加商品</a></li>' +
 						   '</ul>' +
 						   '</div>'	+
-						   '<div class="adspot_tab_action"><a title="取消" class="adspot_edit_btn" href="#">取 消</a><a title="确定" class="adspot_edit_btn" href="#">确 定</a></div>' +
+						   '<div class="adspot_tab_action"><a title="取消" class="adspot_edit_btn cancel">取 消</a><a title="确定" class="adspot_edit_btn submit">确 定</a></div>' +
 						   '</div>';
 			return spotForm;
 			
@@ -132,6 +177,7 @@
 		    var addDotSpotDiv = "<div class='adspot_btn_add_dot'><a>添加锚点</a></div>";
 		    var addEditSpotFormDiv = getAddEditSpotFormDiv();
 		    
+		    img.addClass("adSpotImgWrap");
 			img.wrap(divWrap);
 			wrapperProductDots(img);
 			
@@ -140,6 +186,7 @@
 			img.after(addEditSpotFormDiv);
 			
 			bindAddSpotDivClick(img);
+			bindSoptHoverEvent(img);
 		}
 	
 				
